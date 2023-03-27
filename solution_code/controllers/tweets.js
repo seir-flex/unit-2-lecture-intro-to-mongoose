b609b0c46a27d6ddfaeb4d20cde2c2cddf1c781e
Joe Keohan <jkeohan@gmail.com>
2023-03-27 17:17:42 -0400

    updates

--- solution_code/controllers/tweets.js
+++ solution_code/controllers/tweets.js
@@ -1,46 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const Tweet = require('../models/tweet.js');
-const tweetSeed = require('../models/tweetSeed.js')
-
 
 // Create
 router.post('/', async (req, res) => {
-	const tweet = await Tweet.create(req.body);
-	res.send(tweet);
+	res.send('tweet post route');
 });
 
 // Index
 router.get('/', async (req, res) => {
-	const tweets = await Tweet.find({});
-	res.send(tweets);
+	res.send('tweet index route');
 });
 
 // Seed
 router.get('/seed', async (req, res) => {
-    await Tweet.deleteMany({})
-    await Tweet.create(tweetSeed);
-    res.redirect('/tweets');
-})
+	res.redirect('tweet seed route');
+});
 
 // Show
 router.get('/:id', async (req, res) => {
-	const tweet = await Tweet.findById(req.params.id);
-	res.send(tweet);
+	res.send('tweet show route');
 });
 
 // Delete
 router.delete('/:id', async (req, res) => {
-	const tweet = await Tweet.findByIdAndDelete(req.params.id);
-	res.send({ success: true, tweet });
+	res.send('tweet delete route');
 });
 
 // Update
 router.put('/:id', async (req, res) => {
-	const tweet = await Tweet.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-	});
-	res.send(tweet);
+	res.send('tweet update route');
 });
 
-module.exports = router
+module.exports = router;
--- solution_code/server.js
+++ solution_code/server.js
@@ -1,22 +1,15 @@
 // Dependencies
 const express = require('express');
 const app = express();
-const PORT = 3000;
-const mongoose = require('./db/connection');
-const tweetSeed = require('./models/tweetSeed.js');
-
-// Middleware
-// Body parser middleware: it creates req.body
-app.use(express.urlencoded({ extended: false }));
-
+const PORT = 3000
 
 
 app.get('/', (req, res) => {
-	res.send('default route');
-});
+    res.send('default route')
+})
 
-const tweetsController = require('./controllers/tweets')
-app.use('/tweets', tweetsController)
+const tweetsController = require('./controllers/tweets');
+app.use('/tweets', tweetsController);
 
 // Listener
 app.listen(PORT, () => console.log(`express is listening on port: ${PORT}`));
